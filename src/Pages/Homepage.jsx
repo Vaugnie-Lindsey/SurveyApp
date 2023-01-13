2054014357702a1adb0fc4fc1eb9688232c50eea
Cwoodard2 <cawooda@siue.edu>
2023-01-13 09:04:32 -0600

    Fixes tailwind issue, should be  in seperate branch to prevent issues with trevor initially

--- src/Pages/Homepage.jsx
+++ src/Pages/Homepage.jsx
@@ -1,9 +1,8 @@
 import React from "react";
-
 const Homepage = () => {
     return(
         <>
-            <h1 className="text-4xl">Homepage</h1>
+            <h1 className="text-green-500">Homepage</h1>
         </>
     );
 };
