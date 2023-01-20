e34639315e2fadfaedb2856679463800462268a7
Cwoodard2 <cawooda@siue.edu>
2023-01-19 23:47:52 -0600

    Even more CSS updates

--- src/Pages/SurveyPage.jsx
+++ src/Pages/SurveyPage.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, {useRef} from "react";
 import MCQuestion from "../Components/MCQuestion";
 import NumberQuestion from "../Components/NumberQuestion";
 import ScaleQuestion from "../Components/ScaleQuestion";
