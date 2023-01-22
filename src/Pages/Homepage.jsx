148d79ce769e080549df960c97a91f66490f1ded
Trevor <Trevorauten@gmail.com>
2023-01-22 17:22:40 -0600

    Basic test for adding a doc to our firebase firestorm db

--- src/Pages/Homepage.jsx
+++ src/Pages/Homepage.jsx
@@ -1,11 +1,40 @@
-import React from "react";
-import Users from './Components/Test/index';
-//import Firebase, { FirebaseContext } from '../components/firebase';
+import React, {useState} from "react";
+import db from "../firebase";
+import { collection, addDoc } from "firebase/firestore";
 
 const Homepage = () => {
+    const [users, setUser] = useState("");
+
+    const submit = (e) => {
+        e.preventDefault();
+       
+        try {
+            addDoc(collection(db, "Main"), {
+                ParentID: users,    
+            });
+            console.log("Document written with ID: ", users);
+          } catch (e) {
+            console.error("Error adding document: ", e);
+          }
+    };
+
     return(
-        <Users />
+        
+          <div>
+                <h1>Testing data input...</h1>
+                <label>User Name:</label>
+                
+                <input
+                  type='text'
+                  name='uid'
+                  value={users}
+                  onChange={(e) => setUser(e.target.value)}
+                />
+               <button onClick={submit}>Submit</button>
+              
+            </div>
     );
+        
 };
 
 export default Homepage;
