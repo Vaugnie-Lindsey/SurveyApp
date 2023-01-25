3993f4388e3d0598681b79f3cd137574f5c031b9
Trevor <Trevorauten@gmail.com>
2023-01-25 15:15:39 -0600

    Added basic functionality to check db for valid code/token

--- src/Pages/Homepage.jsx
+++ src/Pages/Homepage.jsx
@@ -4,13 +4,22 @@ import { getFirestore, doc, getDoc } from "firebase/firestore";
 import db from "../firebase";
 
 const Homepage = () => {
-  const codeValid = (async () => {
+  const codeValid = (async (passedToken) => {
+
     const docRef = doc(db, "Codes", "invitation_tokens");
 
     try {
       const snap = await getDoc(docRef)
-      
-      console.log(snap.data());
+      var i;
+      for(i = 0; i < snap.data()['token'].length; i++) {
+        var token = snap.data()['token'][i];
+        if(passedToken == token) {
+          console.log("Token is valid")
+          // Link to informed consent page
+        }
+      }
+      // Link to error page
+
       } catch(error) {
         console.log(error)
         }
@@ -23,11 +32,13 @@ const Homepage = () => {
     <div className="flex flex-col justify-center items-center gap-5 dark:bg-slate-900 dark:text-white min-h-screen">
       <h1 className="text-green-500 text-4xl">Have a Coffee on Us</h1>
       <input
+        id='form'
         className="border-2 border-red-500 p-1 rounded-md"
         placeholder="Please input your referal code here"
+        required
       ></input>
       <Link to="/InformedConsent">
-        <button className="bg-green-500 rounded-md text-white p-3" onClick={() => codeValid()}>
+        <button className="bg-green-500 rounded-md text-white p-3" onClick={() => codeValid(document.getElementById("form").value)}>
           Continue
         </button>
       </Link>
