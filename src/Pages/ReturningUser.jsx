6a2f7d2649c5187db3511af1bef7b3a9963a1af4
Cwoodard2 <cawooda@siue.edu>
2023-01-31 12:00:34 -0600

    Added returning user page

---
+++ src/Pages/ReturningUser.jsx
@@ -0,0 +1,18 @@
+import React from "react";
+
+const ReturningUser = () => {
+  return (
+    <>
+      <div className="flex flex-col items-center min-h-screen dark:bg-slate-900 dark:text-white p-16 gap-10">
+        <h1 className="text-3xl text-green-500">
+          Hmm... Looks like something went wrong
+        </h1>
+        <p>
+          The Link provided contains an invalid authentification code. Please
+          contact the person who referred you and confirm you have the correct
+          link
+        </p>
+      </div>
+    </>
+  );
+};
