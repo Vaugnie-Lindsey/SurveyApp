bef8eede23fc020401756cfbff2dc193acd8a63a
Cwoodard2 <cawooda@siue.edu>
2023-03-20 11:11:47 -0500

    Small commit

--- src/Pages/InvalidToken.jsx
+++ src/Pages/InvalidToken.jsx
@@ -3,8 +3,8 @@ import { Link } from "react-router-dom";
 
 const InvalidToken = () => {
   return (
-    <div className="flex flex-col items-center min-h-screen p-16 gap-10">
-      <h1 className="text-3xl text-appOrange">
+    <div className="flex flex-col items-center min-h-screen p-16 gap-10 signika">
+      <h1 className="text-3xl text-appPurple">
         Hmm... Looks like something went wrong
       </h1>
       <p>
@@ -13,9 +13,9 @@ const InvalidToken = () => {
         link
       </p>
       <Link to="/ReturningUser">
-        <p className="text-appOrange underline">
+        <button className="bg-appPink text-appPurple font-bold p-3 rounded-3xl hover:text-appPink hover:bg-appPurple active:translate-y-3 transition-all text-center flex flex-row items-center justify-center">
           Already Completed the Survey?
-        </p>
+        </button>
       </Link>
     </div>
   );
